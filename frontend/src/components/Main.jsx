572da7e95a16ccd15644266ebde0f5d5e08e3f0a
Anastasiia <seliverstova.anastasia3@yandex.ru>
2022-06-30 18:20:57 +0300

    Update NavBar, and Main page

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import NavBar from './NavBar';
 
 function Main() {
     return (
-        <>
-        <h1 className="mb-1">Игра</h1>
-        <NavBar />
-        &nbsp;&nbsp;&nbsp;
-        <a className="btn btn-primary btn-xl" href="/auth/register">Регистрация</a>&nbsp;&nbsp;&nbsp;
-        <Link className="btn btn-primary btn-xl" to="/login">Войти</Link>
-      </>
+        <div>
+        <h1 className="mb-1" style={{textAlign: 'center'}}>Я хочу сыграть с тобой в одну игру</h1>
+        </div>
     );
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom';
 function NavBar() {
     return (
             <nav>
-    <div className="nav-wrapper">
-      <Link to="/main" className="brand-logo">My own Game</Link>
+    <div className="nav-wrapper" style={{backgroundColor:'black'}}>
+      <Link to="/main" className="logo">My own Game</Link>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><Link to="sass.html">Sass</Link></li>
-        <li><Link to="badges.html">Components</Link></li>
-        <li><Link to="collapsible.html">JavaScript</Link></li>
+        <li><Link to="/">Вернуться в реальность</Link></li>
+        <li><Link to="/register">Получить допуск к игре</Link></li>
+        <li><Link to="/login">Войти в игру</Link></li>
       </ul>
     </div>
   </nav>
