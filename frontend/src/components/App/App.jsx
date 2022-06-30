80807de0542e4a2f49461da4f24f2bfba4098c4c
danil <danil88tmn@gmail.com>
2022-06-30 19:28:08 +0300

    Add logic of layour of questions

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,31 +1,26 @@
 import { Provider } from 'react-redux';
-import {
-     BrowserRouter,
-     Routes,
-     Route,
-     } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Main from '../Main';
 import Register from '../Register';
 import Login from '../Login';
 import NavBar from '../NavBar';
-
+import store from '../../store';
+import GameBoard from '../GameBoard';
 
 function App() {
-    return (
-        <>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/' element={<Login />} />
-
-        </Routes>
-      </>
-    );
+  return (
+    <Provider store={store}>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<GameBoard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Provider>
+  );
 }
 
 export default App;
--- frontend/src/components/GameBoard.jsx
+++ frontend/src/components/GameBoard.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { QUESTIONS_GET } from '../store/questions/actionsTypes';
+import style from './GameBoard.module.css';
+
+function getThemes(questions) {
+  const themes = {};
+  questions.forEach((question) => {
+    if (themes.hasOwnProperty(question.theme)) {
+      themes[question.theme].push(question);
+    } else {
+      themes[question.theme] = [question];
+    }
+  });
+  const topics = [];
+  for (let theme in themes) {
+    topics.push({ theme, questions: themes[theme] });
+  }
+  return topics;
+}
 
 function GameBoard(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  const questions = useSelector((state) => state.questions.questions);
+  const dispatch = useDispatch();
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    fetch('/questions')
+      .then((result) => result.json())
+      .then((questions) => {
+        const newTopics = getThemes(questions);
+        setTopics(newTopics);
+        dispatch({ type: QUESTIONS_GET, payload: questions });
+      });
+  }, [dispatch]);
+
+  //   const handleClick =
+
+  return (
+    <div className={style.parent}>
+      {topics &&
+        topics.map((topic, id) => (
+          <div key={id}>
+            <h2>{topic.theme}</h2>
+            {topic.questions.map((question) => (
+              <p key={question.id}>{question.score}</p>
+            ))}
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default GameBoard;
