4626191d13af1504454b4569c7b43b17e3304abb
danil <danil88tmn@gmail.com>
2022-07-01 15:46:57 +0300

    Add memory of questions

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Main({user}) {
-    return (
+function Main() {
+  const user = useSelector((state) => state.auth);
+  return (
+    <div>
+      <h1 className="mb-1" style={{ textAlign: 'center', color: '#ffffff' }}>
+        Я хочу сыграть с тобой в одну игру
+      </h1>
+      {!user.login && (
+        <>
+          <form action="/error">
+            <button type="submit" className="buttonMain">
+              Жми!
+            </button>
+          </form>
+        </>
+      )}
+      {user.login && (
         <div>
-        <h1 className="mb-1" style={{textAlign: 'center', color: '#ffffff'}}>Я хочу сыграть с тобой в одну игру</h1>
-         {!user && (
-            <>
-        <form action="/error">
-         <button type="submit"  className="buttonMain">Жми!</button>   
-         </form>  
-         </>
-         )}
-         {user && ( 
-            <div>
-            <form action="/game/play">
-              <button type="submit"  className="buttonMain">Давай поиграем!</button>   
-            </form>  
-            </div> 
-         )} 
-       </div>
-    );
+          <form action="/game/play">
+            <button type="submit" className="buttonMain">
+              Давай поиграем!
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Main;
